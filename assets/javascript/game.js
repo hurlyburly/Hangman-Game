9c689787a9e27ba67c51a918a60a85df0e8c20e9
Kalsang Bhutia <35473365+hurlyburly@users.noreply.github.com>
2018-03-25 15:36:46 -0500

    commenting out the steps needed and building js object

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+// Javascript:
+//         3. h2 Array of phrases that pop up in response to correct answer for hangman
+//         4. Array of images that pop up in response to correct answer for hangman
+//         5. Count and display amount of words guessed correctly
+//         6. Populate new word for the game and hide it from user
+//         7. Display correct amount of spaces for the next word in the array. Display correct letters in spaces for each word as correct letters are guessed.
+//         8. Countdown number of guesses made remaining from set number of guesses before game over. Don't count down on guesses for the same letter already made in the round. 
+//         9. Display letters already guessed in the current round. Don't display guesses already made more than once.
+var hangmanGame={
+    word:[],
+    userInput:[],
+    winningPhrases:[],
+    winningPics:[],
+    winningMusic:[],
+    countdownGuesses:[],
+    lettersGuessed:[]
+
+
+}
