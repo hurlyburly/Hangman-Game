7cfa6bbff267ed050cd907bbff35c0813a9ee939
Kalsang Bhutia <35473365+hurlyburly@users.noreply.github.com>
2018-03-30 22:06:33 -0500

    adding some userinteraction. still deciding on how javascript needs to be laid out

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,39 +4,101 @@
 //         5. Count and display amount of words guessed correctly
 //         6. Populate new word for the game and hide it from user
 //         7. Display correct amount of spaces for the next word in the array. Display correct letters in spaces for each word as correct letters are guessed.
-//         8. Countdown number of guesses made remaining from set number of guesses before game over. Don't count down on guesses for the same letter already made in the round. 
+//         8. Countdown number of guesses made remaining from set number of guesses before game over. Don't count down on guesses for the same letter already made in the round.
 //         9. Display letters already guessed in the current round. Don't display guesses already made more than once.
 
+//If using the rps game, make sure you change the code so that each key press isn't counted as a new game.
 
-//If using the rps game, make sure you change the code so that each key press isn't counted as a new game. 
+var wins = 0;
+var guessesRemaining = 9;
+var words = [
+  "Corgi",
+  "Terrier",
+  "Pug",
+  "Dalmatian",
+  "Dachshund",
+  "Beagle",
+  "Labrador"
+];
+var phrases = [
+  "Corgis are Cute!",
+  "Terriers are terrific!",
+  "Pugs are pugnacious!",
+  "Dalmations are dolls!",
+  "Dachshunds are delightful!",
+  "Beagles are beautiful!",
+  "Labradors are lovable!"
+];
+var images = [
+  "assets/images/corgi.jpeg",
+  "assets/images/terrier.jpeg",
+  "assets/images/pug.jpeg",
+  "assets/images/dalmatians.jpeg",
+  "assets/images/dachschund.jpeg",
+  "assets/images/beagle.jpeg",
+  "assets/images/labrador.jpeg"
+];
 
-var hangman={
-    
-    word:[],
-    //phrases id in html h2 "phrases"
-    phrases:[],
-    //winningPic id in html div class col-md-8 id-"winningPic"
-    winningPic:[],
-    //winningMusic id in html div class col-md-8 id-"winningMusic"
-    winningMusic:[],
-    userInput: function(){
-    var userGuess = document.getElementById('userInput');
-    
-    console.log(userIput);
-    
-    },
+var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+ 
+var userStart = false;
+//start game by pressing any key
 
-    winningPhrases:function(){
-        
-    }, 
+document.onkeyup = function(event) {
+  if (userStart) {
+    var words = [
+      "Corgi",
+      "Terrier",
+      "Pug",
+      "Dalmatian",
+      "Dachshund",
+      "Beagle",
+      "Labrador"
+    ];
+    var guessWord = words[Math.floor(Math.random() * words.length)];
+    var blank = "";
+    for (var i = 0; i < guessWord.length; i++) {
+      blank = blank + "_ ";
+    }
+    document.querySelector("#word").innerHTML = blank;
+  
 
-    countdownGuesses:function(){
-        
-    },  
-    
-    lettersGuessed:function(){
-        
-    },  
+  } else {
+    userStart = true;
+    //testing if this works properly: remove this console.log when submitting final version of homework
+    console.log("you pressed " + event.key);
 
+    var html = "You've started the game!";
+    document.querySelector("#userInput").innerHTML = html;
+  }
+};
 
-}
+// var hangman={
+
+//     start:,
+//     word:[],
+//     //phrases id in html h2 "phrases"
+//     phrases:[],
+//     //winningPic id in html div class col-md-8 id-"winningPic"
+//     winningPic:[],
+//     //winningMusic id in html div class col-md-8 id-"winningMusic"
+//     winningMusic:[],
+//     userStart:function(){
+
+//     },
+
+//     winningPhrases:function(){
+
+//     },
+
+//     countdownGuesses:function(){
+
+//     },
+
+//     lettersGuessed:function(){
+
+//     },
+
+// }
+
+// console.log(userGuess);
